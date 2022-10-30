bf60744335587f471e0c3d30998d6af390d89784
Jeewantha-Madubashana <nishanhf2021@gmail.com>
2022-10-30 18:34:25 +0530

    Added temp server and some comments

--- server/index.js
+++ server/index.js
@@ -15,6 +15,12 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
+const serverTemp = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
