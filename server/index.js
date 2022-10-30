5bd116c0d64424949082f6bf82711c12c5b866f7
Jeewantha-Madubashana <thilithahf2021@gmail.com>
2022-10-30 17:57:33 +0530

    Added imports

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const http = require('http');
 
+const express = require('express')
+const router = express.Router()
+const authController = require('../../controllers/auth.controller')
+const validator = require('express-validation')
+const { create } = require('../../validations/user.validation')
+const auth = require('../../middlewares/authorization')
+
 const port = 3000;
 
 const server = http.createServer((req, res) => {
