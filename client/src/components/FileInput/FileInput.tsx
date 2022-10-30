442bb9c0cae58760e8dce71aad29ef112d28afc3
Sathuta-Ranasingha <sathutahf22@gmail.com>
2022-10-30 22:48:01 +0530

    Image on change

--- client/src/components/FileInput/FileInput.tsx
+++ client/src/components/FileInput/FileInput.tsx
@@ -6,7 +6,19 @@ export default function FileInput(props: { onChange?: CallableFunction }) {
         <input
             type="file"
             className="file-upload-input"
-
+            onChange={(e) => {
+                if (e.target.files) {
+                    var imgURL = toBase64(e.target.files?.[0]).then(
+                        (url: any) => {
+                            props.onChange?.(url);
+                            
+                        }
+                    );
+                } else {
+                    props.onChange?.(null);
+                    
+                }
+            }}
         />
     );
 }
