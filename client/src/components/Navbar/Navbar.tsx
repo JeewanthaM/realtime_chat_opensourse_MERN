49db63cd80ab9606dcf5e3964a9d2d0f96c31a96
Jeewantha-Madubashana <93342803+TharinduHF21@users.noreply.github.com>
2022-10-30 11:50:05 +0530

    Refactor navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -4,7 +4,6 @@ import AddIcon from "../../assets/images/icons/add.svg";
 import GearIcon from "../../assets/images/icons/gear.svg";
 import FilterIcon from "../../assets/images/icons/filter.svg";
 import ArrowDown from "../../assets/images/icons/down-arrow.svg";
-
 import "./Navbar.scss";
 
 export default function Navbar(props: {
@@ -19,7 +18,30 @@ export default function Navbar(props: {
     return (
         <div className="navbar-wrap">
             <div className="d-flex flex-row tab-section">{props.children}</div>
+            <div className="d-flex flex-row justify-content-around align-items-end">
+                {props.hideFilter
+                    ? (<Fragment />)
+                    : (<div onClick={() => { props.onFilterButtonClick?.(); }} className="icon filter">
+                        <img src={FilterIcon} className="" />
+                        <p style={{ margin: "0rem 1rem 0rem 0.7rem", fontFamily: "monospace" }}>Filter</p>
+                        <img src={ArrowDown} className="" />
+                    </div>)
+                }
 
+                {props.hideSettings
+                    ? (<Fragment />)
+                    : (<div onClick={() => { props.onSettingsButtonClick?.(); }} className="icon gear d-flex justify-content-center">
+                        <img src={GearIcon} className="gear-icon" />
+                    </div>)
+                }
+
+                {props.hideAdd
+                    ? (<Fragment />)
+                    : (<div onClick={() => { props.onAddButtonClick?.(); }} className="icon add d-flex justify-content-center">
+                        <img src={AddIcon} className="add-icon" />
+                    </div>)
+                }
+            </div>
         </div >
     );
 }
